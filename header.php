eff8313322ad89e2c13d79734da31be6d32aef57
James <james@studiothis.co.uk>
2025-02-28 21:46:45 +0000

    init

---
+++ header.php
@@ -0,0 +1,14 @@
+<html <?php language_attributes(); ?>>
+
+	<head>
+		
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<meta name="theme-color" content="#000000">
+		<title><?php wp_title('-'); ?></title>
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/favicon.png">
+		<?php wp_head(); ?>
+		
+	</head>
+	theme updater

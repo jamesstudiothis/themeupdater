445cfb961f766fc48454d619dbe3277e8a0f1183
James <james@studiothis.co.uk>
2025-02-28 21:57:41 +0000

    Theme Update

--- functions.php
+++ functions.php
@@ -1,1 +1,24 @@
 <?php
+
+function custom_theme_check_for_update($transient) {
+	if (empty($transient->checked)) {
+		return $transient;
+	}
+
+	$response = wp_remote_get(get_template_directory_uri() . '/theme-update.json');
+	if (!is_wp_error($response) && wp_remote_retrieve_response_code($response) == 200) {
+		$theme_data = json_decode(wp_remote_retrieve_body($response), true);
+		$current_theme = wp_get_theme();
+
+		if (version_compare($current_theme->get('Version'), $theme_data['new_version'], '<')) {
+			$transient->response[$current_theme->get_stylesheet()] = (object) [
+				'theme'       => $theme_data['theme'],
+				'new_version' => $theme_data['new_version'],
+				'url'         => $theme_data['url'],
+				'package'     => $theme_data['package']
+			];
+		}
+	}
+	return $transient;
+}
+add_filter('pre_set_site_transient_update_themes', 'custom_theme_check_for_update');
